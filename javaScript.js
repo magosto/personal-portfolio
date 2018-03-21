88fb5d539489f75c780566a065f7691fbf074e2b
Matthew Agosto <matt@Matthews-MacBook-Pro.local>
2018-03-21 02:26:20 -0400

    created nav bar that is responsive to screen size. Added click functon to JS file, and added icon to the nav list

--- javaScript.js
+++ javaScript.js
@@ -1,11 +1,21 @@
 //Add NAV transitioning when scrolled
 $(window).scroll(function () {
   if ($(window).scrollTop() >= 350) {
-    $('.navbar ul').css('background','white');
-    $('.navbar a').css('color','black');
+    $('.navBar ul').css('background','white');
+    $('.navBar a').css('color','black');
 
   } else {
     $('.navbar ul').css('background','transparent');
     $('.navbar a').css('color','white');
   }
 });
+
+//nav menu icon onclick
+function navClick() {
+    var x = document.getElementById("topNav");
+    if (x.className === "navBar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navBar";
+    }
+}
