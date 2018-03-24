458d814a96ee7d7fa3346d25483013b52cc1970d
Matthew Agosto <matt@Matthews-MacBook-Pro.local>
2018-03-24 16:18:28 -0400

    Added headers to mobiles version of porfolio section

--- javaScript.js
+++ javaScript.js
@@ -1,11 +1,10 @@
 /*Fade in background*/
-/*/Fade in background*/
-
 $(window).on("load", function() {
   $("#loading-page").fadeOut(1000);
   $(".hero-image").fadeIn(2000);
   $("#hidden-body").fadeIn(2000);
 });
+/*/Fade in background*/
 
 /*Add NAV transitioning when scrolled*/
 $(window).scroll(function () {
