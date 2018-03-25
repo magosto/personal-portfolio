168cae964a49598b697575b0a4d28f044f25a969
Matthew Agosto <matt@Matthews-MacBook-Pro.local>
2018-03-25 11:43:53 -0400

    Removed Nav items from mark up, css, and JS. Until the page grows larger, the nav does not have much necessity here.

--- javaScript.js
+++ javaScript.js
@@ -5,27 +5,3 @@ $(window).on("load", function() {
   $("#hidden-body").fadeIn(2000);
 });
 /*/Fade in background*/
-
-/*Add NAV transitioning when scrolled*/
-$(window).scroll(function () {
-  if ($(window).scrollTop() >= 350) {
-    $('.navBar ul').css('background','white');
-    $('.navBar a').css('color','black');
-
-  } else {
-    $('.navBar ul').css('background','transparent');
-    $('.navBar a').css('color','white');
-  }
-});
-/*/Add NAV transitioning when scrolled*/
-
-/*nav menu icon onclick*/
-function navClick() {
-    var x = document.getElementById("topNav");
-    if (x.className === ".navBar") {
-        x.className += " responsive";
-    } else {
-        x.className = ".navBar";
-    }
-}
-/*/nav menu icon onclick*/
